a4302da671e13ee044844077b46ad5a6d2350ef4
tainaramar <taina@ramarventures.com>
2026-04-21 20:22:29 -0300

    container ok

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll(".header-menu a")
+
+function ativarlinks(link){
+    console.log(link);
+    const url = location.href;
+    const href = link.href;
+
+
+    url.includes
+    if(url === href){
+        console.log(link)
+    }
+}
+
+links.forEach(ativarlinks)
